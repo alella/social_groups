047d0cfe2bb6f37f30ab6cd7106b3b829d8fd374
alella <ashok.lella@gmail.com>
2018-04-20 13:26:47 -0500

    Fetch all articile ttiles for a story domain

--- fandom_extract.py
+++ fandom_extract.py
@@ -1,9 +1,10 @@
-"""
+# -*- coding: utf-8 -*-
+""" 
 extract information from fandom.wikia
 """
-
 import json
 import requests
+import urllib
 from pprint import pprint
 
 def getArticleInfo(domain,id):
@@ -46,3 +47,27 @@ def name2json(domain,names,fname):
         pprint(items[name])
     json.dump(items,open(fname,'w'))
 
+
+
+def fetch_all_article_titles(domain):
+    article_titles = []
+    offset = ""
+    fname = "char_info/{}_articles.json".format(domain)
+    while True:
+        url = "http://{0}.wikia.com/api/v1/Articles/List/?offset={1}".format(domain,
+                                                                             offset)
+        r = requests.get(url).json()
+        article_titles += [t['title'] for t in r['items']]
+        if 'offset' not in r:
+            break
+        offset = r['offset']
+        offset = offset.replace(u"´", "").replace(u"ü", "").replace(u"№", "").replace(u"’", "")
+        offset = urllib.quote_plus(offset)
+        print "articles =", len(article_titles), offset
+
+    json.dump(article_titles,open(fname,'w'))
+    return article_titles
+
+
+# domain = "themagicians"
+# print fetch_all_article_titles(domain)
